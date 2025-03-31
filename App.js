d4ee751cd397191dff7fc95d0a4d8182b2c50733
anumalisettyshanmukhi <147909848+anumalisettyshanmukhi@users.noreply.github.com>
2025-03-31 12:10:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,24 +2,11 @@ import React from 'react';
 import Heading from './ReactProject/WeatherApplication/Heading';
 import Weather from './ReactProject/WeatherApplication/Weather';
 
-
-// import Login from './Forms/Login';
-// import Signup from './Forms/Signup';
-// import Password from './Forms/Password';
-
 function App() {
   return (
     <>
     <Heading />
     <Weather />  
-    {/* <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Login />}/>
-      <Route path='/signup' element={<Signup />}/>
-      <Route path='/password' element={<Password />}/>
-    </Routes>
-    </BrowserRouter>
-   */}
     </>
   );
 }
